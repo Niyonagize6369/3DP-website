1314ad16f605dc28bbf8447aa23c03e1d24878a0
Your Name <niyonagizerachel10@email.com>
2025-08-22 12:12:19 +0200

    footer is edited

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -12,8 +12,8 @@ const footerLinks = [
 ];
 
 const resourceLinks = [
-  { href: "/service", label: "Service" },
   { href: "/about", label: "About Us" },
+  { href: "/service", label: "Service" },
 ];
 
 export const Footer = () => {
@@ -107,8 +107,8 @@ export const Footer = () => {
 
         <div className="mt-12 border-t border-gray-700 pt-8 flex justify-center">
           <p className="text-white text-sm text-center">
-            &copy; {new Date().getFullYear()} <span className="text-yellow-500">3DP</span>.rw All rights
-            reserved.
+            &copy; {new Date().getFullYear()}{" "}
+            <span className="text-yellow-500">3DP</span>.rw All rights reserved.
           </p>
         </div>
       </div>
